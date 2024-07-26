de927784b51e4d8d13794c35e8e232e8ada8c264
Santhosh Kumar K <s-k6@ti.com>
2024-07-26 14:57:09 +0530

    spi: cadence-quadspi: Enable PHY for data writes
    PHY is tuned with optimal tuning point which allows controller to
    run at higher speeds. Hence, increase the data writes' throughput in
    OSPI/QSPI NAND flashes by enabling PHY for data writes to the NAND
    flash devices.
    
    The aim is to enable PHY only for the OSPI/QSPI NAND data writes,
    so, exclude other operations like register writes to NAND flashes,
    register and data writes to NOR flashes by introducing a check for
    the 'n_tx' (op->data.nbytes) value before enabling.
    
    Currently, OSPI/QSPI NOR's highest page size is 512 bytes, so, check
    whether 'n_tx' is greater than or equal to 1024.
    
    Signed-off-by: Santhosh Kumar K <s-k6@ti.com>

--- drivers/spi/spi-cadence-quadspi.c
+++ drivers/spi/spi-cadence-quadspi.c
@@ -1912,6 +1912,9 @@ static int cqspi_indirect_write_execute(struct cqspi_flash_pdata *f_pdata,
 	if (cqspi->apb_ahb_hazard)
 		readl(reg_base + CQSPI_REG_INDIRECTWR);
 
+	if (n_tx >= SZ_1K && f_pdata->use_phy)
+		cqspi_phy_enable(f_pdata, true);
+
 	while (remaining > 0) {
 		size_t write_words, mod_bytes;
 
@@ -1952,6 +1955,9 @@ static int cqspi_indirect_write_execute(struct cqspi_flash_pdata *f_pdata,
 		goto failwr;
 	}
 
+	if (n_tx >= SZ_1K && f_pdata->use_phy)
+		cqspi_phy_enable(f_pdata, false);
+
 	/* Disable interrupt. */
 	writel(0, reg_base + CQSPI_REG_IRQMASK);
 
@@ -1963,6 +1969,9 @@ static int cqspi_indirect_write_execute(struct cqspi_flash_pdata *f_pdata,
 	return 0;
 
 failwr:
+	if (n_tx >= SZ_1K && f_pdata->use_phy)
+		cqspi_phy_enable(f_pdata, false);
+
 	/* Disable interrupt. */
 	writel(0, reg_base + CQSPI_REG_IRQMASK);
 
