4d1aa40f6d5a6c20f66a995a737d12ad4f940807
Sai Sree Kartheek Adivi <s-adivi@ti.com>
2025-06-23 11:07:08 +0530

    dmaengine: ti: k3-udma: refactor resource setup functions
    The implementation of setup_resources, bcdma_setup_resources and
    pktdma_setup_resources is largely shared between all K3 UDMA variants
    with the only major difference being SCI resources setup. So,
    - Move the functions to k3-udma-common.c.
    - Split SCI resource setup for bcdma and pktdma into separate functions
      in variant specific driver (k3-udma.c).
    - Add function pointers for setup_sci_resources in udma_dev and call
      them as part of the actual resource setup implementations in
      k3-udma-common.c to retain the existing functionality.
    
    This refactor improves code reuse and maintainability across multiple
    variants.
    
    No functional changes intended.
    
    Signed-off-by: Sai Sree Kartheek Adivi <s-adivi@ti.com>

--- drivers/dma/ti/k3-udma.h
+++ drivers/dma/ti/k3-udma.h
@@ -349,6 +349,8 @@ struct udma_dev {
 	int (*stop)(struct udma_chan *uc);
 	int (*reset_chan)(struct udma_chan *uc, bool hard);
 	void (*decrement_byte_counters)(struct udma_chan *uc, u32 val);
+	int (*bcdma_setup_sci_resources)(struct udma_dev *ud);
+	int (*pktdma_setup_sci_resources)(struct udma_dev *ud);
 };
 
 struct udma_desc {
@@ -670,6 +672,11 @@ void bcdma_free_bchan_resources(struct udma_chan *uc);
 struct udma_bchan *__udma_reserve_bchan(struct udma_dev *ud, enum udma_tp_level tpl, int id);
 struct udma_tchan *__udma_reserve_tchan(struct udma_dev *ud, enum udma_tp_level tpl, int id);
 struct udma_rchan *__udma_reserve_rchan(struct udma_dev *ud, enum udma_tp_level tpl, int id);
+
+int udma_setup_resources(struct udma_dev *ud);
+int bcdma_setup_resources(struct udma_dev *ud);
+int pktdma_setup_resources(struct udma_dev *ud);
+int setup_resources(struct udma_dev *ud);
 
 /* Direct access to UDMA low lever resources for the glue layer */
 int xudma_navss_psil_pair(struct udma_dev *ud, u32 src_thread, u32 dst_thread);
